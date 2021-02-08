8f4eda6df2183ceb481fd7a3183e2a24ec2dc15a
Osiris Román <osirisr1994@gmail.com>
2021-02-07 22:27:42 -0500

    Updating the web app to template engines.

--- routes/admin.js
+++ routes/admin.js
@@ -1,33 +1,22 @@
 const express = require('express');
-/*A nodejs library that let us construct 
-the full path of our files*/
+
 const path = require('path');
 
 const rootDir = require('../utils/path');
 const router = express.Router();
 
-// /admin/add-product => GET
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-  /* The res.sendFile() Function is a Nodejs function that 
-  allow us to send any kind of files as a response 
-  to a given incoming request.
-  the path.join() Function is the path library tool 
-  that allow us to build the full path for the file 
-  that we want to send. This full path is built 
-  depending on the operative system where we are 
-  running our server. It is necessary to remember 
-  that linux and windows build the path differently 
-  Windows build the path using '\' while Linux use '/'.
-  __dirname is a global variable that stores the 
-  absolute path in our operative system to this folder
-  */
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-// /admin/product => POST
 router.post('/add-product', (req, res) => {
-  console.log('body: ', req.body);
+  products.push({ title: req.body.productname });
   res.redirect('/');
 });
 
-module.exports = router;
+module.exports = {
+  router,
+  products,
+};
--- server.js
+++ server.js
@@ -2,38 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const app = express();
-
 const adminRoutes = require('./routes/admin');
 const publicRoutes = require('./routes/shop');
 
-/*When handling POST resquest the body comes encoded 
-Then, we decode the body using the body-parser module 
-and we do it at the beginning of all middlewares in 
-order to let us use the parsed value in other tasks.
-If we don't decode the body then the body attribute 
-added by express will be undefined
-*/
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-/*
-Users usually cannot acces to the files placed in the 
-project. Nevertheless, there is always a public folder 
-which contains static files that needs to be served in 
-responses. Then the properly way to serve these files 
-is using a middleware that check if there is a request 
-of an static file and look for this file in the 
-specified defined public folder.
-*/
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*
-With this new addition we filter request with a 
-common starting path for all routes that use it 
-and are placed in a given file. In this example 
-we filter the ones that comes to our /admin/ path.
-*/
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes.router);
 app.use(publicRoutes);
 
 app.use((req, res, next) => {
