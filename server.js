8b43547ae220475bf0ba11355984da67e75ed599
Osiris Román <osirisr1994@gmail.com>
2021-02-09 18:48:00 -0500

    Updating the project to EJS template engine

--- server.js
+++ server.js
@@ -4,19 +4,10 @@ const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const publicRoutes = require('./routes/shop');
-const expressHBS = require('express-handlebars');
 
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHBS({
-    layoutsDir: 'views/layouts',
-    defaultLayout: 'main-layout',
-    extname: 'hbs',
-  })
-);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: true }));
